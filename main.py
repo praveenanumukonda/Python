1840385b45c6e511dfe2872c49577a868d83e744
aprav <praveenanumukonda@gmail.com>
2021-08-14 22:16:08 +0530

    Hungry Code with fruits

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ if hungry == 'yes':
     print("Eat Biryani")
 else:
     print('Do work')
+
+i = 0
+
+fruits = ["apple", "banana", "cherry"]
+fruit = input("Enter your fav fruit: ")
+for x in fruits:
+    print(x)
+    if x != fruit:
+        continue
+    break
